f589c52b3131ba51fcbf671b34bf2c38dbbeaaf4
Matheus Boltech <matheus@email.com>
2025-08-15 18:01:59 -0300

    tela teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
     <title>Bem-vindo</title>
     @vite(['resources/css/app.css'])
     <link rel="stylesheet" href="omeoakeosae">
 </head>
-<body>
-   <!-- Adicione isso no <head> do seu HTML se estiver usando Heroicons via CDN -->
+<body class="bg-[url('/public/Images/fundo.png')] md:bg-fixed">
+  
 <script src="https://unpkg.com/@heroicons/vue@2.0.16/24/solid/star.js"></script>
-
-<!-- Card de Filme com ícone de estrela -->
-<div class="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:shadow-xl transition-shadow duration-300">
-  <img class="w-full h-64 object-cover" src="https://image.tmdb.org/t/p/original/4VtkIaj76TpQNfhDHXQDdT9uBN5.jpg" alt="Poster do Filme">
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">O Grande Filme</div>
-    <p class="text-gray-700 text-base">
-      Aventura • Ficção Científica • 2025
-    </p>
-  </div>
-  <div class="px-6 pt-2 pb-4 flex items-center justify-between">
-    <div class="flex items-center space-x-1 text-yellow-500">
-      <!-- Ícone de estrela -->
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current" viewBox="0 0 20 20">
-        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.957a1 1 0 00.95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.37 2.448a1 1 0 00-.364 1.118l1.286 3.957c.3.921-.755 1.688-1.538 1.118l-3.37-2.448a1 1 0 00-1.175 0l-3.37 2.448c-.783.57-1.838-.197-1.538-1.118l1.286-3.957a1 1 0 00-.364-1.118L2.075 9.384c-.783-.57-.38-1.81.588-1.81h4.162a1 1 0 00.95-.69l1.286-3.957z" />
-      </svg>
-      <span class="font-semibold text-sm">4.8/5</span>
-    </div>
-    <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-      Ver Detalhes
-    </button>
-  </div>
+<div class="bg-grey-blue/80 w-40 h-screen fixed rounded-r-lg visibility">
+    <button class="bg-primary hover:bg-blue w-30 h-15 rounded-lg mt-70 ml-5 shadow-x1/20 text-white font-bold ">Início</button>
 </div>
- </body>
+<div class="ml-40 h-auto overflow-y-auto flex flex-col min-w-200 sn:100 md:flex justify-center items-center ">
+    <div class="md:flex">  
+      <div class="bg-gradient-to-r from-[#7ed957]/70 to-[#007e50] rounded-lg my-20 w-200 h-130 max-h-130 sm:min-h-100">
+
+      </div>
+    </div>
+</div>
+
+
+</body>
 </html>
