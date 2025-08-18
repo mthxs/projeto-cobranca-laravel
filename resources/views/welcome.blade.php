4fa80fa8d8e31b82da120c06eb8ed17bb8eb9fdd
mthxs <matheusguilherme06062005@gmail.com>
2025-08-18 07:20:16 -0300

    atualização do html estilzando botao, e fazendo tela para celular

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,26 @@
 <body class="bg-[url('/public/Images/fundo.png')] md:bg-fixed">
   
 <script src="https://unpkg.com/@heroicons/vue@2.0.16/24/solid/star.js"></script>
-<div class="bg-grey-blue/80 w-40 h-screen fixed rounded-r-lg visibility">
-    <button class="bg-primary hover:bg-blue w-30 h-15 rounded-lg mt-70 ml-5 shadow-x1/20 text-white font-bold ">Início</button>
+<div class="bg-grey-blue/80 w-40 h-screen fixed rounded-r-lg hidden lg:flex flex-col items-center justify-center">
+    <div class="flex flex-col gap-10 items-center">
+        <button class="bg-primary hover:bg-sky-700 focus:outline-2 focus:outline-offset-5 focus:outline-sky-700 active:bg-sky-700 w-32 h-12 rounded-full drop-shadow-xl/50 text-white font-bold">Início</button>
+        <button class="bg-primary hover:bg-sky-700 focus:outline-2 focus:outline-offset-5 focus:outline-sky-700 active:bg-sky-700 w-32 h-12 rounded-full drop-shadow-xl/50 text-white font-bold">Botão 2</button>
+        <button class="bg-primary hover:bg-sky-700 focus:outline-2 focus:outline-offset-5 focus:outline-sky-700 active:bg-sky-700 w-32 h-12 rounded-full drop-shadow-xl/50 text-white font-bold">Botão 3</button>
+        <button class="bg-primary hover:bg-sky-700 focus:outline-2 focus:outline-offset-5 focus:outline-sky-700 active:bg-sky-700 w-32 h-12 rounded-full drop-shadow-xl/50 text-white font-bold">Botão 4</button>
+    </div>
 </div>
-<div class="ml-40 h-auto overflow-y-auto flex flex-col min-w-200 sn:100 md:flex justify-center items-center ">
-    <div class="md:flex">  
-      <div class="bg-gradient-to-r from-[#7ed957]/70 to-[#007e50] rounded-lg my-20 w-200 h-130 max-h-130 sm:min-h-100">
-
+<div class="lg:ml-40 h-auto overflow-y-auto flex flex-col min-w-200 sn:100 md:flex justify-center items-center ">
+    <div class="md:flex justify-center h-full">  
+        <div class="bg-gradient-to-r from-[#7ed957]/70 to-[#007e50] backdrop-blur-[5px] rounded-lg mx-5 my-20 p-5 w-150 h-300 max-w-150 lg:mx-0 lg:my-30 lg:p-8 lg:max-w-250 lg:w-250 lg:h-140">
       </div>
     </div>
 </div>
 
+<buttom id="menuBtn" class="lg:hidden fixed top-4 left-4 ">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-18 h-18">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>
+</buttom>
 
 </body>
 </html>
